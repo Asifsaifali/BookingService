778657d434f21f88c204c2924517501a0683fc93
Asif Saif Ali <asif263848@gmail.com>
2023-05-28 22:40:29 +0530

    Added Routes setup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
-
-
+const ApiRoute = require("./routers/index");
+const db = require("./models/index");
 
 const setupServer = () => {
   app.use(bodyparser.urlencoded({ extended: true }));
   app.use(bodyparser.json());
+
+  app.use("/api", ApiRoute);
   app.listen(PORT, () => {
     console.log(`Server is running at Port ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true }).then(() => {
+        console.log("Database is synced");
+      });
+    }
   });
 };
 
